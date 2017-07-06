eea678ecc88fb0597ae5b80a54f921b12c6c1c05
Dmitry Shechtman <damnian@gmail.com>
2017-07-07 00:37:36 +0300

    Add solution

---
+++ src/Net.Chdk.Meta.Model.Base/SourceData.cs
@@ -0,0 +1,9 @@
+﻿namespace Net.Chdk.Meta.Model
+{
+    public abstract class SourceData<TSource>
+        where TSource : SourceData<TSource>
+    {
+        public string Platform { get; set; }
+        public string Revision { get; set; }
+    }
+}
